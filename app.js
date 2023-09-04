da8ce00783dfbf83325a0de872a452cd64690814
Ayofe Faheez <ayofefaheez@gmail.com>
2023-09-03 21:17:42 -0800

    desktop-design-completed

--- app.js
+++ app.js
@@ -11,13 +11,115 @@ const required = document.querySelectorAll('.aside__step--one__required')
 const nextPlanBtn = document.querySelector('.aside__step--one__nextPlan')
 const nextAddonBtn = document.querySelector('.aside__step--two__nextAddon')
 const nextSumBtn = document.querySelector('.aside__step--three__nextSum')
+const backInfoBtn = document.querySelector('.aside__step--two__backInfo')
+const backPlanBtn = document.querySelector('.aside__step--three__backPlan')
+const backAddonBtn = document.querySelector('.aside__step--four__backAddon')
+const confirmBtn = document.querySelector('.aside__step--four__nextCon')
+const changePlan = document.querySelector('.change')
+const completed = document.querySelector('.completed')
 const InputName = document.querySelector('.input__log--name')
 const InputEmail = document.querySelector('.input__log--email')
 const InputTel = document.querySelector('.input__log--tel')
 
+
 const arcade = document.querySelector('.arcade')
 const advanced = document.querySelector('.advanced')
 const pro = document.querySelector('.pro')
+
+const service = document.querySelector('.service')
+const serviceChoice = document.querySelector('.service--choice')
+const storage = document.querySelector('.storage')
+const storageChoice = document.querySelector('.storage--choice')
+const profile = document.querySelector('.profile')
+const profileChoice = document.querySelector('.profile--choice')
+
+const toggleBtn = document.querySelector('.toggle__btn')
+const monthlyText = document.querySelector('.toggle__text--monthly')
+const yearlyText = document.querySelector('.toggle__text--yearly')
+const yearlyPrice = document.querySelectorAll('.yearly--price')
+const price = document.querySelectorAll('.price') 
+
+const AddonYearlyPrice = document.querySelectorAll('.addon__yearly--price') 
+const AddonMonthlyPrice = document.querySelectorAll('.addon__monthly--price') 
+
+let num1 = document.querySelector('.num1')
+let num2 = document.querySelector('.num2')
+let num3 = document.querySelector('.num3')
+let num4 = document.querySelector('.num4')
+let sum = document.querySelector('.sum')
+
+const choiceDate = document.querySelectorAll('.choiceDate')
+
+
+console.log(sum)
+
+
+const toggleFunc = function () {
+    for (i = 0; i < yearlyPrice.length; i++) {
+        var x = yearlyPrice[i]
+        if (x.style.display === 'block') {
+            x.style.display = "none";
+            for (j = 0; j < price.length; j++) {
+                price[j].classList.remove('hidden')
+            }
+            for (k = 0; k < AddonMonthlyPrice.length; k++) {
+                AddonMonthlyPrice[k].classList.remove('hidden')
+            }
+            for (l = 0; l < AddonYearlyPrice.length; l++) {
+                AddonYearlyPrice[l].classList.add('hidden')
+            }
+
+            document.querySelector('.price--arcade').textContent = '$9/mo'
+            document.querySelector('.price--advanced').textContent = '$12/mo'
+            document.querySelector('.price--pro').textContent = '$15/mo'
+
+                num2.textContent = "1"
+                num3.textContent = '2'
+                num4.textContent = '2'
+
+                for (m = 0; m < choiceDate.length; m++) {
+                    choiceDate[m].textContent = 'mo'
+                }
+
+                document.querySelector('.totalDate').textContent = '(per month)'
+
+            monthlyText.style.color = 'hsl(213, 96%, 18%)'
+            yearlyText.style.color = 'hsl(229, 24%, 87%)' 
+        } else {
+            x.style.display = "block";
+            for (j = 0; j < price.length; j++) {
+                price[j].classList.add('hidden')
+            }
+            for (k = 0; k < AddonMonthlyPrice.length; k++) {
+                AddonMonthlyPrice[k].classList.add('hidden')
+            }
+            for (l = 0; l < AddonYearlyPrice.length; l++) {
+                AddonYearlyPrice[l].classList.remove('hidden')
+            }
+
+            document.querySelector('.price--arcade').textContent = '$90/yr'
+            document.querySelector('.price--advanced').textContent = '$120/yr'
+            document.querySelector('.price--pro').textContent = '$150/yr'
+
+
+            num2.textContent = "10"
+            num3.textContent = '20'
+            num4.textContent = '20'
+
+            for (m = 0; m < choiceDate.length; m++) {
+                choiceDate[m].textContent = 'yr'
+            }
+
+            document.querySelector('.totalDate').textContent = '(per year)'
+
+            monthlyText.style.color = 'hsl(229, 24%, 87%)'
+            yearlyText.style.color = 'hsl(213, 96%, 18%)'
+        }
+    }
+    
+    
+}
+
 
 stepOne.classList.add('num')
 
@@ -26,19 +128,25 @@ nextPlanBtn.addEventListener('click', function() {
         if (InputName.value === "" && InputEmail.value === "" && InputTel.value === "") {
             for (let i = 0; i < required.length; i++) {
             required[i].classList.remove('hidden')
+            setTimeout(function() {
+                required[i].classList.add("hidden")}, 3000 
+            )
             }
         }
         else if (InputName.value === "") {
             for (let i = 0; i < required.length; i++) {
                 required[0].classList.remove('hidden')
+                setTimeout(() => {required[0].classList.add("hidden")}, 3000)
             }
         } else if (InputEmail.value === "") {
             for (let i = 0; i < required.length; i++) {
                 required[1].classList.remove('hidden')
+                setTimeout(() => {required[1].classList.add("hidden")}, 3000)
             }
         } else if (InputTel.value === "") {
             for (let i = 0; i < required.length; i++) {
                 required[2].classList.remove('hidden')
+                setTimeout(() => {required[2].classList.add("hidden")}, 3000)
             }
         } else if (InputName.value !== "" && InputEmail.value !== "" && InputTel.value !== "") {
             stepOne.classList.remove('num')
@@ -63,6 +171,133 @@ nextSumBtn.addEventListener('click', function() {
     stepFour.classList.add('num')
     secThree.classList.add('hidden')
     secFour.classList.remove('hidden')
+
+    sum.textContent = `${Number(num1.textContent) + Number(num2.textContent) + Number(num3.textContent) + Number(num4.textContent)}`
+    console.log(sum.textContent)
+})
+
+backInfoBtn.addEventListener('click', function () {
+    stepOne.classList.add('num')
+    stepTwo.classList.remove('num')
+    secOne.classList.remove('hidden')
+    secTwo.classList.add('hidden')
+})
+
+backPlanBtn.addEventListener('click', function () {
+    stepOne.classList.remove('num')
+    stepTwo.classList.add('num')
+    stepThree.classList.remove('num')
+    secTwo.classList.remove('hidden')
+    secThree.classList.add('hidden')
+})
+
+
+
+backAddonBtn.addEventListener('click', function () {
+    stepOne.classList.remove('num')
+    stepTwo.classList.remove('num')
+    stepThree.classList.add('num')
+    stepFour.classList.remove('num')
+    secThree.classList.remove('hidden')
+    secFour.classList.add('hidden')
+})
+
+changePlan.addEventListener('click', function () {
+    stepOne.classList.remove('num')
+    stepTwo.classList.add('num')
+    stepThree.classList.remove('num')
+    stepFour.classList.remove('num')
+    secTwo.classList.remove('hidden')
+    secFour.classList.add('hidden')
+})
+
+confirmBtn.addEventListener('click', function() {
+    // stepOne.classList.remove('num')
+    // stepTwo.classList.remove('num')
+    // stepThree.classList.remove('num')
+    // stepFour.classList.add('num')
+    secFour.classList.add('hidden')
+    completed.classList.remove('hidden')
+})
+
+toggleBtn.addEventListener('click', function () {
+    toggleFunc();
+})
+
+arcade.addEventListener('click', function () {
+    if (document.querySelector('.price--arcade').textContent === '$9/mo') {
+        document.querySelector('.plan__choice').textContent = 'Arcade (monthly)' 
+        document.querySelector('.plan__price').textContent = '$9/mo' 
+        num1.textContent = 9
+    } else if (document.querySelector('.price--arcade').textContent === '$90/yr') {
+        document.querySelector('.plan__choice').textContent = 'Arcade (yearly)' 
+        document.querySelector('.plan__price').textContent = '$90/yr' 
+        num1.textContent = 90
+    }
+    console.log (num1.textContent)
+})
+
+advanced.addEventListener('click', function () {
+    if (document.querySelector('.price--advanced').textContent === '$12/mo') {
+        document.querySelector('.plan__choice').textContent = 'Advanced (monthly)' 
+        document.querySelector('.plan__price').textContent = '$12/mo' 
+        num1.textContent = 12
+    } else if (document.querySelector('.price--advanced').textContent === '$120/yr') {
+        document.querySelector('.plan__choice').textContent = 'Advanced (yearly)' 
+        document.querySelector('.plan__price').textContent = '$120/yr' 
+        num1.textContent = 120
+    }
+    console.log (num1.textContent)
+})
+
+pro.addEventListener('click', function () {
+    if (document.querySelector('.price--pro').textContent === '$15/mo') {
+        document.querySelector('.plan__choice').textContent = 'Pro (monthly)' 
+        document.querySelector('.plan__price').textContent = '$15/mo' 
+        num1.textContent = 15
+    } else if (document.querySelector('.price--pro').textContent === '$150/yr') {
+        document.querySelector('.plan__choice').textContent = 'Pro (yearly)' 
+        document.querySelector('.plan__price').textContent = '$150/yr' 
+        num1.textContent = 150
+    }
+    console.log (num1.textContent)
+})
+
+service.addEventListener('click', function () {
+    if (this.checked) {
+        serviceChoice.classList.remove('hidden')
+        num2.textContent = "1"
+        console.log (num2.textContent)
+    } else {
+        serviceChoice.classList.add('hidden')
+        num2.textContent = "0"
+        console.log (num2.textContent)
+    }
+})
+
+storage.addEventListener('click', function () {
+    if (this.checked) {
+        storageChoice.classList.remove('hidden')
+        num3.textContent = '2'
+        console.log (num3.textContent)
+    } else {
+        storageChoice.classList.add('hidden')
+        num3.textContent = '0'
+        console.log (num3.textContent)
+    }
+    
+})
+
+profile.addEventListener('click', function () {
+    if (this.checked) {
+        profileChoice.classList.remove('hidden')
+        num4.textContent = '2'
+        console.log (num4.textContent)
+    } else {
+        profileChoice.classList.add('hidden')
+        num4.textContent = '0'
+        console.log (num4.textContent)
+    }
 })
 
 
